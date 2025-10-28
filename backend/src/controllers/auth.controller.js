6500e2218d4210ffdce075bcf81846db4469eaa0
software.inkhub <software.inkhub@gmail.com>
2025-10-28 23:20:26 +0530

    signup code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -34,8 +34,16 @@ export const signup = async (req,res)=>{
             password: hashedPassword,
          });
          if(newUser){
-           generateToken(newUser._id, res);
-           await newUser.save();
+
+            //after cr 
+         //   generateToken(newUser._id, res);
+         //   await newUser.save();
+
+           //presise user first then issue and cookie 
+           const savedUser = await newUser.save();
+           generateToken(savedUser._id, res);
+
+           
               res.status(201).json({
                 _id: newUser._id,
                 fullName: newUser.fullName,
