cdca7396ac6c7e5943997f8eb5d0abba9a7f1a92
software.inkhub <software.inkhub@gmail.com>
2025-10-29 01:55:10 +0530

    login and logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -71,3 +71,33 @@ export const signup = async (req,res)=>{
     
     }
 };
+
+export const login = async (req,res)=>{
+   const {email, password} = req.body;
+   try {
+      const user = await User.findOne({email: email});
+      if (!user) return res.status(400).json({message: "Invalid credentials"});
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({message: "Invalid credentials"});
+
+      generateToken(user._id, res);
+      res.status(200).json({
+         _id: user._id,
+         fullName: user.fullName,
+         email: user.email,
+         profilePic: user.profilePic,
+      });
+
+   } catch (error) {
+      console.log("something went wrong in login controller", error);
+      res.status(500).json({message: "Server error"});
+   }
+};
+
+
+
+export const logout = (_,res)=>{
+   res.cookie("jwt", "", {maxAge: 0});
+   res.status(200).json({message: "Logged out successfully"});
+};
