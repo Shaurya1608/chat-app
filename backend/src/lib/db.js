ecc4ff68fe382a4d8853ab3a75a8c7dedd303061
software.inkhub <software.inkhub@gmail.com>
2025-10-29 01:20:39 +0530

    welcome-email-refactoring too

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
+import { ENV } from './env.js';
 
 export const connectDB =async () => {
     try {
-        const {MONGO_URL} = process.env;
+        const {MONGO_URL} = ENV;
         if (!MONGO_URL) throw new Error('MONGO_URL is not defined in environment variables');
     
-        const conn =await mongoose.connect(process.env.MONGO_URL,)
+        const conn =await mongoose.connect(ENV.MONGO_URL,)
         console.log('mongoDB connected:', conn.connection.host);
     } catch (error) {
         console.log('Error in DB connection', error);
